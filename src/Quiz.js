c19f76dd22dbcb3c813ce5956d5ecff5c7a5c838
D Taylor <deondredtaylor@gmail.com>
2018-09-26 14:32:45 -0500

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,6 +6,9 @@ let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
 
+    showNextQuestion(){
+        this.setState((state) => { return {quiz_position: state.quiz_position+1}})
+    }
     constructor(props) {
         super(props);
         this.state = { quiz_position: 1};
@@ -17,7 +20,7 @@ class Quiz extends Component {
         return (
             <div>
               { (isQuizEnd) ? <QuizEnd /> :
-              <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}/> }
+              <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}/> }
             </div>
         )
     }
